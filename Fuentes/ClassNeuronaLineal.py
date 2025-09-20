7154db3b491ee35d907da975d58ed1ca00419a9c
Mateo Romero <materomero04@gmail.com>
2025-09-20 18:34:12 -0300

    Practica 3 - Terminada

--- Fuentes/ClassNeuronaLineal.py
+++ Fuentes/ClassNeuronaLineal.py
@@ -6,6 +6,7 @@ from IPython import display
 
 from grafica import *
 
+
 class NeuronaLineal(object):
     """
     Parameters
@@ -20,7 +21,7 @@ class NeuronaLineal(object):
         1 si dibuja -  0 si no
     title : list con 2 elementos
         titulos de los ejes - sólo 2D
-        
+
     Attributes
     -----------
     w_ : 1d-array
@@ -28,13 +29,17 @@ class NeuronaLineal(object):
     errors_ : list
         Number of misclassifications (updates) in each epoch.
     """
-    def __init__(self, alpha=0.01, n_iter=50, cotaE=10E-07, random_state=None, draw=0, title=['X1','X2']):
+
+    def __init__(self, alpha=0.01, n_iter=50, cotaE=10E-07, random_state=None, draw=0, title=['X1', 'X2'], no_bias=False):
         self.alpha = alpha
         self.n_iter = n_iter
         self.cotaE = cotaE
-        self.random_state = random_state #-- asignar el valor 1 para fijar la semilla por defecto es aleatorio
+        # -- asignar el valor 1 para fijar la semilla por defecto es aleatorio
+        self.random_state = random_state
         self.draw = draw
         self.title = title
+        self.no_bias = no_bias
+        self.iterations_ = 0
 
     def fit(self, X, y):
         """Fit training data.
@@ -52,47 +57,52 @@ class NeuronaLineal(object):
 
         # graficar la recta
         if (self.draw):
-            ycol=y.reshape(-1,1)
-            puntos = np.concatenate((X,ycol), axis=1)
+            ycol = y.reshape(-1, 1)
+            puntos = np.concatenate((X, ycol), axis=1)
             T = np.zeros(X.shape[0])
-            
+
         rgen = np.random.RandomState(self.random_state)
 
         # self.w_ = rgen.normal(loc=0.0, scale=0.01,size=1 + X.shape[1])
 
-        self.w_ = rgen.uniform(-0.5, 0.5, size= X.shape[1]) 
+        self.w_ = rgen.uniform(-0.5, 0.5, size=X.shape[1])
         self.b_ = rgen.uniform(-0.5, 0.5)
         self.errors_ = []
         ph = 0  # manejador de la recta mientras se dibuja
         ErrorAnt = 0
         ErrorAct = 1
-        
+
         i = 0
-        while ((i<self.n_iter) and (np.abs(ErrorAnt- ErrorAct) > self.cotaE)):
+        while ((i < self.n_iter) and (np.abs(ErrorAnt - ErrorAct) > self.cotaE)):
             ErrorAnt = ErrorAct
             ErrorAct = 0
             for xi, target in zip(X, y):
-                
+
                 errorXi = (target - self.predict(xi))
                 update = self.alpha * errorXi
                 self.w_ += update * xi
                 self.b_ += update
-                
+
                 ErrorAct += errorXi**2
-                
+
             self.errors_.append(ErrorAct)
-            
+
             # graficar la recta
             if (self.draw):
-                ph = dibuPtosRecta(puntos,T, np.array([self.w_, -1],dtype=object), self.b_, self.title, ph)
-            
+                ph = dibuPtosRecta(puntos, T, np.array(
+                    [self.w_, -1], dtype=object), self.b_, self.title, ph)
+
             i = i + 1
+        self.iterations_ = i
         return self
 
     def net_input(self, X):
         """Calculate net input"""
-        return np.dot(X, self.w_) + self.b_
+        if not self.no_bias:
+            return np.dot(X, self.w_) + self.b_
+        else:
+            return np.dot(X, self.w_)
 
     def predict(self, X):
         """Return class label after unit step"""
-        return self.net_input(X) 
+        return self.net_input(X)
